7e542646b45f7769396651f2167f0fee6f48e998
slavaaaaa88 <d4896869@gmail.com>
2025-07-30 21:32:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/global.css";
-import usePageView from "./hooks/usePageView";
-
-const Root = () => {
-  usePageView();
-  return <App />;
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Root />
+    <App />
   </BrowserRouter>
 );
